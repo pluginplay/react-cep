269448fbb6300ce5328023897ca235fa5b7259fe
Samuel Marks <sam@sammarks.me>
2018-07-21 23:59:49 -0400

    chore: Fix linter initially.

---
+++ README.md
@@ -0,0 +1,3 @@
+# React CEP
+
+More information coming soon!
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,9 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { render } from 'react-dom';
-import HelloWorld from './components/HelloWorld';
+import { render } from 'react-dom'
+import HelloWorld from './components/HelloWorld'
 
-const root = document.getElementById('root');
+const root = document.getElementById('root')
 
 render((
   <HelloWorld />
-), root);
+), root)
