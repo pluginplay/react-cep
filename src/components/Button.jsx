7e695446127ba7a5007e0b65f4cf5c814f2d1849
Samuel Marks <sam@sammarks.me>
2018-12-01 19:45:50 -0500

    feat: Disable styles for buttons.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -11,16 +11,22 @@ export default styled.button`
   font-size: ${props => props.theme.ae.baseFontSize}pt;
   outline: none;
   box-sizing: border-box;
-  transition: background 0.1s ease-in-out, border-color 0.1s ease-in-out, transform 0.1s ease-in-out;
+  transition: background 0.1s ease-in-out, border-color 0.1s ease-in-out, transform 0.1s ease-in-out, 
+    opacity 0.1s ease-in-out;
+  opacity: 1;
   transform: scale(1);
   cursor: pointer;
   display: inline-block;
-  &:hover, &:focus, &:active {
+  &:not(:disabled):hover, &:not(:disabled):focus, &:not(:disabled):active {
     border-color: ${props => props.theme.ae.systemHighlightColor};
     background: ${props => props.theme.background.darker};
   }
-  &:active { 
+  &:not(:disabled):active { 
     transform: scale(0.95);
     background: ${props => props.theme.ae.panelBackgroundColor};
+  }
+  &:disabled {
+    cursor: default;
+    opacity: 0.5;
   }
 `
